e2254e1e2a5138d620851c31bafcdde6a2e7c0f0
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-09 05:48:29 +0000

    add import tests

--- test.c
+++ test.c
@@ -6,33 +6,143 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 03:33:41 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/11/09 03:57:17 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/09 05:41:45 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libdict/libdict.h"
+#include "libvect/libvect.h"
+#include <string.h>
 #include <stdio.h>
 
 #define ADD(k, v) dict_str_add(d, k, v)
-#define LOOKUP(k) \
+#define LOOKUP(k, v) test++; \
 	ent = dict_str_lookup(&d, k); \
-	printf("%s = %.*s\n", k, (int)ent->val.used, ent->val.data);
+	vect_mset_end(&ent->val, '\0', 1); \
+	if (ent && !strcmp(v, ent->val.data)) ok++;
+#define DEL(k, v)  test++; \
+	dict_del(&d, k); \
+	ent = dict_str_lookup(&d, k); \
+	if (!ent) ok++;
+#define IMPORT(k, v); \
+	dict_str_import(d, k"="v, '=');
 
 
 void		build_dict(t_dict *d)
 {
 	dict_str_init(d);
-	ADD("banana", "fruit");
-	ADD("lazy", "me");
+#define TEST ADD
+	TEST("banana", "fruit");
+	TEST("lazy", "me");
+	TEST("Trump", "bad");
+	TEST("flowers are", "blue");
+	TEST("hashmaps are", "cool");
+	TEST("bleu blanc", "rouge");
+	TEST("answer", "42");
+	TEST("logic", "fail");
+	TEST("holy", "thunder");
+	TEST("baby", "rage");
+	TEST("dummy", "ymmud");
+	TEST("don't", "bully");
+	TEST("Lolita", "Vladimir Nabokov");
+	TEST("Cent ans de solitude", "Gabriel Garcia Marquez");
+	TEST("La mort du roi Tsongor", "Laurent Gaude");
+}
+
+void		import_dict(t_dict *d)
+{
+#undef TEST
+#define TEST IMPORT
+	TEST("banana", "fruit");
+	TEST("lazy", "me");
+	TEST("Trump", "bad");
+	TEST("flowers are", "blue");
+	TEST("hashmaps are", "cool");
+	TEST("bleu blanc", "rouge");
+	TEST("answer", "42");
+	TEST("logic", "fail");
+	TEST("holy", "thunder");
+	TEST("baby", "rage");
+	TEST("dummy", "ymmud");
+	TEST("don't", "bully");
+	TEST("Lolita", "Vladimir Nabokov");
+	TEST("Cent ans de solitude", "Gabriel Garcia Marquez");
+	TEST("La mort du roi Tsongor", "Laurent Gaude");
 }
 
 int			main(void)
 {
 	t_dict d;
 	t_dict_ent *ent;
+	int test;
+	int ok;
 
 	build_dict(&d);
-	LOOKUP("banana");
-	LOOKUP("lazy");
+	test = 0;
+	ok = 0;
+#undef TEST
+#define TEST LOOKUP
+	TEST("banana", "fruit");
+	TEST("lazy", "me");
+	TEST("Trump", "bad");
+	TEST("flowers are", "blue");
+	TEST("hashmaps are", "cool");
+	TEST("bleu blanc", "rouge");
+	TEST("answer", "42");
+	TEST("logic", "fail");
+	TEST("holy", "thunder");
+	TEST("baby", "rage");
+	TEST("dummy", "ymmud");
+	TEST("don't", "bully");
+	TEST("Lolita", "Vladimir Nabokov");
+	TEST("Cent ans de solitude", "Gabriel Garcia Marquez");
+	TEST("La mort du roi Tsongor", "Laurent Gaude");
+	printf("OK: %d\\%d\n", ok, test);
+	printf("%lu used, %lu total\n", d.used, d.total);
+	test = 0;
+	ok = 0;
+#undef TEST
+#define TEST DEL
+	TEST("banana", "fruit");
+	TEST("lazy", "me");
+	TEST("Trump", "bad");
+	TEST("flowers are", "blue");
+	TEST("hashmaps are", "cool");
+	TEST("bleu blanc", "rouge");
+	TEST("answer", "42");
+	TEST("logic", "fail");
+	TEST("holy", "thunder");
+	TEST("baby", "rage");
+	TEST("dummy", "ymmud");
+	TEST("don't", "bully");
+	TEST("Lolita", "Vladimir Nabokov");
+	TEST("Cent ans de solitude", "Gabriel Garcia Marquez");
+	TEST("La mort du roi Tsongor", "Laurent Gaude");
+	printf("OK: %d\\%d\n", ok, test);
+	printf("%lu used, %lu total\n", d.used, d.total);
+	test = 0;
+	ok = 0;
+#undef TEST
+#define TEST LOOKUP
+	import_dict(&d);
+	TEST("banana", "fruit");
+	TEST("lazy", "me");
+	TEST("Trump", "bad");
+	TEST("flowers are", "blue");
+	TEST("hashmaps are", "cool");
+	TEST("bleu blanc", "rouge");
+	TEST("answer", "42");
+	TEST("logic", "fail");
+	TEST("holy", "thunder");
+	TEST("baby", "rage");
+	TEST("dummy", "ymmud");
+	TEST("don't", "bully");
+	TEST("Lolita", "Vladimir Nabokov");
+	TEST("Cent ans de solitude", "Gabriel Garcia Marquez");
+	TEST("La mort du roi Tsongor", "Laurent Gaude");
+	printf("OK: %d\\%d\n", ok, test);
+	printf("%lu used, %lu total\n", d.used, d.total);
+
+	dict_free(&d);
 	return (0);
 }
