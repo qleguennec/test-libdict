a2e51a01cb3a49c25b4df9bfe2acfc8818ca6ac2
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-11 03:00:27 +0000

    backup

--- gen.sh
+++ gen.sh
@@ -1,10 +1,11 @@
 DIR="$HOME"
 FMT='TEST("%i", "%f");\\\n'
 FMT_OSX="TEST(\"%i\", \"%f\");\\"
+TYPE=d
 
 echo "#define PRE \\"
 if [[ "$(uname)" == "Linux" ]]; then
-	find $DIR -printf "$FMT"
+	find $DIR -type $TYPE -printf "$FMT"
 else
-	find $DIR -print0 | xargs -0 stat -f "$FMT_OSX"
+	find $DIR -type $TYPE -print0 | xargs -0 stat -f "$FMT_OSX"
 fi
